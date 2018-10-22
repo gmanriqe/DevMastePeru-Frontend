ed7678da142e0f057b0f073f1cae4edafc472f88
gmanriqe <gmanriqe@gmail.com>
2018-10-22 10:02:08 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,40 @@
+function onReady() {
+
+  $(window).on("load scroll", function() {
+    // Menu
+    var sTop = $(window).scrollTop();
+    var heightMenu= $('.menu').height();
+    navBar = $("#navBar");
+    if (sTop >= heightMenu) {
+      navBar.parent().addClass("fixed-top");
+      navBar.parent().css({
+        "background-color": "black"
+      });
+      navBar.parent().addClass('menu-animated');
+    } else {
+      navBar.parent().removeClass("fixed-top");
+      navBar.parent().css({
+        "background-color": "black"
+      });
+      navBar.parent().removeClass('menu-animated');
+    }
+
+    // Owl Carrousel
+    $('.owl-carousel').owlCarousel();
+
+    // To Top
+    $(window).on('load scroll',function(){
+      var scroll = $(window).scrollTop();
+      if( scroll >= 1400) {
+        $('.to-top').addClass('top-block');
+      }else {
+        $('.to-top').removeClass('top-block');
+      }
+    })
+    $('.to-top').on('click',function(){
+      $('html, body').animate({ scrollTop: 0}, 700)
+    });
+  });
+}
+
+$(document).ready(onReady());
